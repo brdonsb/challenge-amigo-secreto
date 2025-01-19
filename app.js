6be2dc8dbb2fd722bfdc1a9730219231f8a97880
brdonsb@gmail.com <brdonsb@gmail.com>
2025-01-19 11:40:38 -0500

    Inclusão da função para soertear amigo e escrever nome na tela

--- app.js
+++ app.js
@@ -9,4 +9,27 @@ function adicionarAmigo(){
         listaDeAmigos.push(amigo);
         document.getElementById('amigo').value = '';
     }
+    escreveNomesAmigos();
+}
+
+function escreveNomesAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    let contador = 0;
+    lista.innerHTML = '';
+    while(contador < listaDeAmigos.length){
+        lista.innerHTML += `<li>${listaDeAmigos[contador]}</li>`;
+        contador++;
+    }
+}
+
+function sortearAmigo(){
+    let amigoSorteado;
+    let sorteadoResultadoNaTela = document.getElementById('resultado');
+    if(listaDeAmigos == ''){
+        alert('Por favor, insira um nome.');
+    }else{
+        amigoSorteado = Math.floor(Math.random() * listaDeAmigos.length);
+        sorteadoResultadoNaTela.innerHTML = '';
+        sorteadoResultadoNaTela.innerHTML = `<li>O amigo secreto é: ${listaDeAmigos[amigoSorteado]}</li>`;
+    }    
 }
