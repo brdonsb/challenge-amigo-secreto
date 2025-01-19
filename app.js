399e16836b63d446442231e514eea149dbf2a6ea
brdonsb@gmail.com <brdonsb@gmail.com>
2025-01-18 21:55:45 -0500

    adicionar nome à lista de amigos.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+//adiciona nome à lista de amigos.
+function adicionarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    if(amigo == ''){
+        alert('Por favor, insira um nome.');
+    }else{
+        listaDeAmigos.push(amigo);
+        document.getElementById('amigo').value = '';
+    }
+}
